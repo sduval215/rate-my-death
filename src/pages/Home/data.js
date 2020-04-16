ea2f692c42af7897b8eb51a3ded8eeaec34f77cb
Steven Duval Ruilova <dev@ezeldsolutions.com>
2020-04-16 11:59:30 +0200

    implement home page UI layout

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -7,12 +7,45 @@ const slideData = [
     body: null
   },
   {
-    month: 'April',
+    month: 'Apr',
     day: 11,
-    viewers: 1005,
+    viewers: '1,005',
     body: `
       <p>
-        <a>558</a> Americans die of COVID-19, and <a>3.3mm</a> people watch a 
+        <a target="_blank">558</a> Americans die of COVID-19, and <a target="_blank">3.3mm</a> people watch a 
+        White House press briefing with special guest, the MyPillow guy,
+      </p>
+    `
+  },
+  {
+    month: 'Apr',
+    day: 12,
+    viewers: '1,005',
+    body: `
+      <p>
+        <a target="_blank">558</a> Americans die of COVID-19, and <a target="_blank">3.3mm</a> people watch a 
+        White House press briefing with special guest, the MyPillow guy,
+      </p>
+    `
+  },
+  {
+    month: 'Apr',
+    day: 13,
+    viewers: '1,005',
+    body: `
+      <p>
+        <a target="_blank">558</a> Americans die of COVID-19, and <a target="_blank">3.3mm</a> people watch a 
+        White House press briefing with special guest, the MyPillow guy,
+      </p>
+    `
+  },
+  {
+    month: 'Apr',
+    day: 14,
+    viewers: '1,005',
+    body: `
+      <p>
+        <a target="_blank">558</a> Americans die of COVID-19, and <a>3.3mm</a> people watch a 
         White House press briefing with special guest, the MyPillow guy,
       </p>
     `
