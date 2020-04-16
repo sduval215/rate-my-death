4ed2bc98d7d1d33adf58fc3edfcd1f668f0eff49
Steven Duval Ruilova <dev@ezeldsolutions.com>
2020-04-16 09:57:21 +0200

    implement index calendar slide

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,28 +5,13 @@ import Swiper from 'react-id-swiper';
 // COMPONENTS
 import Video from '../../components/Video/Video';
 
+// STATIC DATA
+import { slideData } from './data';
+
 import styles from './Home.module.scss';
 
-const calendarObject = [
-  {
-    month: 'April',
-    day: 11,
-    deaths: 1830,
-    views: '8.8 million'
-  },
-  {
-    month: 'April',
-    day: 11,
-    deaths: 1830,
-    views: '8.8 million'
-  },
-  {
-    month: 'April',
-    day: 11,
-    deaths: 1830,
-    views: '8.8 million'
-  }
-]
+const indexIcon = require('./imgs/index-icon.svg');
+const twitterIcon = require('../../static/imgs/twitter-icon.svg');
 
 class Home extends Component {
   constructor(props){
@@ -48,10 +33,34 @@ class Home extends Component {
       })
     }
   }
+  
+  renderIndexSlide = () => (
+    <div className={[ styles.calendar, styles.indexCalendar ].join(' ')}>
+      <div className={styles.indexHeader}>
+        <img alt="index-icon" src={indexIcon}/>
+          <p>JAN-MAR</p>
+      </div>
+        <div className={styles.indexBody}>
+          <p>
+            For an in-depth timeline of events before Trump's <span>campaign rallies</span>
+            White House press briefings, read this detailed article from the <a href="/">New York Times.</a>
+          </p>
+        </div>
+        <div className={styles.indexFooter}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+          Ut eros urna, sollicitudin quis ornare vel, ultrices sit amet urna.
+        </p>
+        <button type="button">
+          <img alt="twitter" src={twitterIcon}/>
+          TWEET @ TRUMP
+        </button>
+      </div>
+    </div>
+  )
 
   render() {
     const { videoIndex } = this.state;
-    console.log(videoIndex);
     return(
       <div className={styles.pageWrapper}>
         <div className={styles.pageContainer}>
@@ -62,32 +71,26 @@ class Home extends Component {
                 allowTouchMove
                 freeMode
                 freeModeSticky
+                // initialSlide={2}
                 slideNextClass={styles.adjacentSlide}
                 slidePrevClass={styles.adjacentSlide}
                 slideActiveClass={styles.activeSlide}
                 wrapperClass={styles.swiperContainer}
               >
-                <div className={styles.calendar}>
-                  <div className={styles.dateContainer}>
-                    <h1>11</h1>
-                    <h3>April</h3>
-                  </div>
-                  <Video id={videoIndex} />
-                </div>
-                <div className={styles.calendar}>
-                  <div className={styles.dateContainer}>
-                    <h1>12</h1>
-                    <h3>April</h3>
-                  </div>
-                  <Video id={videoIndex} />
-                </div>
-                <div className={styles.calendar}>
-                  <div className={styles.dateContainer}>
-                    <h1>13</h1>
-                    <h3>April</h3>
-                  </div>
-                  <Video id={videoIndex} />
-                </div>
+                {slideData.map((data, index) => {
+                  const { month, day, viewers, body } = data;
+                  // RENDER INDEX SLIDE ON FIRST INDEX
+                  if (index === 0) return this.renderIndexSlide();
+                  return(
+                    <div className={styles.calendar}>
+                      <div className={styles.dateContainer}>
+                        <h1>11</h1>
+                        <h3>April</h3>
+                      </div>
+                      <Video id={videoIndex} />
+                    </div>
+                  )
+                })}
               </Swiper>
             </div>
           </div>
