7ccbef4df5bbfc504a06d6f88022c7b7d69abfb9
Steven Duval Ruilova <dev@ezeldsolutions.com>
2020-04-17 23:53:14 +0200

    add resource href linking

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -16,7 +16,7 @@ const renderResourceItem = (icon, label, route) => (
     </div>
     <div className={styles.link}>
       <p>{label}</p>
-      <a href={route}>
+      <a target="__blank" href={route}>
         <img alt="link" src={link} />
       </a>
     </div>
@@ -27,12 +27,12 @@ const Resources = () => (
   <div className={styles.resourcesContainer}>
     <h3>If you'd like to help, please consider the following:</h3>
     <div className={styles.listContainer}>
-      {renderResourceItem(ppeIcon, 'Donate Personal Protective Equipment (PPE)')}
-      {renderResourceItem(customPPEIcon, 'Make your own PPE')}
-      {renderResourceItem(resource, 'Send Food to a Hospital')}
-      {renderResourceItem(resource, 'Donate Blood or Money to the Red Cross')}
-      {renderResourceItem(resource, 'Save the Postal Service')}
-      {renderResourceItem(resource, 'Support local businesses')}
+      {renderResourceItem(ppeIcon, 'Donate Personal Protective Equipment (PPE)', 'https://thriveglobal.com/categories/first-responders-first/')}
+      {renderResourceItem(customPPEIcon, 'Make your own PPE', 'https://www.youtube.com/watch?v=zm_NmpdgWWA')}
+      {renderResourceItem(resource, 'Send Food to a Hospital', 'https://www.gofundme.com/f/help-us-send-dinner-to-healthcare-heroes-in-nyc')}
+      {renderResourceItem(resource, 'Donate Blood or Money to the Red Cross', 'https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/coronavirus-safety.html')}
+      {renderResourceItem(resource, 'Save the Postal Service', 'https://store.usps.com/store/home')}
+      {renderResourceItem(resource, 'Support local businesses', '')}
     </div>
   </div>
 )
