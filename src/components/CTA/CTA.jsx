a7f7a241d18302cb2fa61d22259accfaa6725829
Steven Duval Ruilova <dev@ezeldsolutions.com>
2020-04-19 14:41:01 +0200

    minor CTA section copy change and restyle

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -5,7 +5,7 @@ import styles from './CTA.module.scss';
 const CTA = () => (
   <div className={styles.CTAContainer}>
     <div>
-      <h2>This should go without saying, please vote in November.</h2>
+      <h2>This should go without saying, but please vote in November:</h2>
       <a target="__blank" href="https://www.usa.gov/register-to-vote">
         <button>
           REGISTER TO VOTE
