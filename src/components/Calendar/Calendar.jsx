86af9e64e753f40e2fcd246e4a1ab474f2ba24aa
Steven Duval Ruilova <sduval215@gmail.com>
2020-04-22 20:29:47 +0200

    refactor calendar section animation logic prevent null possibility

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -30,7 +30,7 @@ class Calendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      slideIndex: slideData.length - 1,
+      slideIndex: slideData.length - 2,
       calendarHeight: window.innerHeight * 0.94
     }
     this.swiper = null;
@@ -51,9 +51,9 @@ class Calendar extends Component {
     // set global swiper class reference for future use
     this.swiper = swiper;
     // animate
-    // setTimeout(() => swiper.slideTo(slideData.length - 1, 2000), 500);
-    // setTimeout(() => this.setState({ slideIndex: slideData.length - 1 }), 2500);
     if(swiper !== null) {
+      // introduction animation
+      setTimeout(() => swiper.slideTo(slideData.length - 1, 1500), 1000);
       // handles listeners for cursor UI updatess
       swiper.on('sliderMove', () => {
         this.setState({ slideIndex: swiper.activeIndex, dragging: true });
